a58530d5034b5ae5b85a77a2575464701ca0181e
Your Name <you@example.com>
2025-05-21 09:26:53 +0800

    cut off docs

--- workspace/yolo-example.py
+++ workspace/yolo-example.py
@@ -2,5 +2,6 @@ import sys
 sys.path.append('')
 
 from ultralytics import YOLO
-model = YOLO('yolo11n.pt')
-model.predict(save=True)
+model = YOLO('yolo11n.yaml',task='detect')
+model.train(data='coco128.yaml', epochs=50)
+model.val()
